49667a990312349f7ca8de9e1de2fcdcba6bf65f
harpreetsingh <ttttester102@gmail.com>
2020-08-03 20:17:12 +0530

    Add discount functionality

--- controller/response.controller.js
+++ controller/response.controller.js
@@ -327,6 +327,7 @@ var mergeObject = function (obj, oldObj) {
  * Get authorization token
  */
 var getAuthoriztionToken = () => {
+    console.log(process.env.APP_TIME_ZONE, process.env.APP_TIME_FORMAT);
     return md5(`${process.env.APP_KEY}${md5(moment(new Date()).tz(process.env.APP_TIME_ZONE).format(process.env.APP_TIME_FORMAT))}`);
 }
 
