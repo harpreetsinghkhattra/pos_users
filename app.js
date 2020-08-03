59b94f979349631f261a57060db9bc2d6166b86c
harpreetsingh <ttttester102@gmail.com>
2020-08-03 19:52:45 +0530

    Add discount functionality

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  console.log("error ===> app", error);
+  console.log("error ===> app", err);
   res.status(err.status || 500);
   res.json({
     status: err.status || 500,
